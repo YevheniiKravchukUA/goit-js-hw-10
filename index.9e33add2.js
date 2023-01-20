d6f9cabd14a0fca12a7781e80d6c6cc3a8142b73
YevheniiKravchukUA <115158804+YevheniiKravchukUA@users.noreply.github.com>
2023-01-20 14:42:02 +0000

    Deploying to gh-pages from @ YevheniiKravchukUA/goit-js-hw-10@4d60fb71407e95261c62d4832a20f076d07043a0 🚀

--- index.9e33add2.js
+++
@@ -1,2 +0,0 @@
-document.querySelector(".country-list"),document.querySelector(".country-info");
-//# sourceMappingURL=index.9e33add2.js.map
--- index.b2f6ba7a.js
+++
@@ -1,2 +0,0 @@
-const{countryList:o,coutryInfo:t}={countryList:document.querySelector(".country-list"),coutryInfo:document.querySelector(".country-info")};
-//# sourceMappingURL=index.b2f6ba7a.js.map
