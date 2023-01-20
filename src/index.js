774f7b2813a4f74fa9ed49b7489af406777b7368
YevheniiKravchukUA <Kravchukmail23@gmail.com>
2023-01-20 09:22:27 +0100

    git initial

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 import './css/styles.css';
 
+const { countryList, coutryInfo } = {
+  countryList: document.querySelector('.country-list'),
+  coutryInfo: document.querySelector('.country-info'),
+};
+
 const DEBOUNCE_DELAY = 300;
